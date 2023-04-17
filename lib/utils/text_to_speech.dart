c664cb30b4438d05b24afb29524da481f9e543c7
Saurrabhh <saurabhkyadav99@gmail.com>
2023-04-17 14:20:19 +0530

    ladki ki awaj aa gayi

--- lib/utils/text_to_speech.dart
+++ lib/utils/text_to_speech.dart
@@ -3,8 +3,8 @@ import 'package:flutter_tts/flutter_tts.dart';
 final FlutterTts flutterTts = FlutterTts();
 speak(String text) async {
   await flutterTts.setLanguage("en-IN");
-  await flutterTts.setPitch(0.56);
-  await flutterTts.setSpeechRate(0.56);
+  await flutterTts.setPitch(1);
+  await flutterTts.setSpeechRate(0.4);
   // Future list = flutterTts.getLanguages;
   // list.then((value) {
   //   print(value);
